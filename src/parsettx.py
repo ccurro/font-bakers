0356ee383a06ce4ac64516504972ed4cfd215e51
Jped <jonathanped@gmail.com>
2019-02-11 17:59:36 -0500

    FIX: added W (#45)
    * FIX: added W
    
    * STY: added a space

--- src/parsettx.py
+++ src/parsettx.py
@@ -4,7 +4,7 @@ from absl import flags, app
 FLAGS = flags.FLAGS
 DESIREDGLPYHS = set([
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
-    'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e',
+    'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e',
     'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
     'u', 'v', 'w', 'x', 'y', 'z', 'zero', 'one', 'two', 'three', 'four',
     'five', 'six', 'seven', 'eight', 'nine', 'space', 'exclam', 'quotedbl',
