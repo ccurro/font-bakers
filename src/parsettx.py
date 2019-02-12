15e663cbb6b346834bcd20598e128b73089200b6
Jped <jonathanped@gmail.com>
2019-02-12 14:01:14 -0500

    Fix glyphs (#46)
    * FIX: added W
    
    * FIX: added the correct GLYPHS
    
    * FIX: i didnt change it before

--- src/parsettx.py
+++ src/parsettx.py
@@ -3,17 +3,15 @@ from absl import flags, app
 
 FLAGS = flags.FLAGS
 DESIREDGLPYHS = set([
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
-    'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e',
-    'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-    'u', 'v', 'w', 'x', 'y', 'z', 'zero', 'one', 'two', 'three', 'four',
-    'five', 'six', 'seven', 'eight', 'nine', 'space', 'exclam', 'quotedbl',
-    'numbersign', 'dollar', 'percent', 'ampersand', 'quotesingle', 'parenleft',
-    'parenright', 'asterisk', 'plus', 'comma', 'hyphen', 'period', 'slash',
-    'colon', 'semicolon', 'less', 'equal', 'greater', 'question', 'at',
-    'bracketleft', 'bracketright', 'asciicircum', 'underscore', 'grave',
-    'braceleft', 'bar', 'braceright', 'asciitilde'
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+        'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c',
+        'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+        'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'zero', 'one', 'two',
+        'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 
+        'exclam', 'numbersign', 'dollar', 'percent', 'ampersand',
+        'asterisk','question', 'at' 
 ])
+
 flags.DEFINE_string("file", None, "input ttx filename")
 flags.mark_flag_as_required("file")
 
